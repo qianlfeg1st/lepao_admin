3f1484caff5c1331b26a3c3244dab93a4713350a
钱立峰 <805042956@qq.com>
2020-11-17 20:39:45 +0800

    fix

--- src/page/prize/detail/index.js
+++ src/page/prize/detail/index.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useContext } from 'react'
 import { useParams, useHistory, Link } from 'react-router-dom'
 import { Table, Button, Modal, Form, Image, InputNumber, DatePicker, Input, message, Col, Row, Pagination, Breadcrumb } from 'antd'
 import { ExclamationCircleOutlined } from '@ant-design/icons'
-import { prize } from '@/api'
+import { prize, company } from '@/api'
 import styles from './index.module.scss'
 import moment from 'moment'
 import { AdminContext } from '@/components/Admin'
@@ -82,17 +82,26 @@ function PrizeDetail () {
     {
       title: '上架时间',
       dataIndex: 'updownTime',
-      width: 120,
+      width: 110,
+    },
+    {
+      title: '状态',
+      dataIndex: 'companyUsed',
+      width: 50,
+      render: (e) => (
+        <>{ e ? '已选用' : '未选用' }</>
+      ),
     },
     {
       title: '操作',
-      width: 115,
+      width: 145,
       render (e) {
 
         return (
           <>
             <Button className="btn" type="primary" onClick={ () => getGoodsDetail(e) }>编辑</Button>
             <Button className="btn" type="danger" onClick={ () => deleted(+e.companyGoodsId) }>移除</Button>
+            <Button className="btn" type="primary" onClick={ () => selectGoods({ companyGoodsId: e.companyGoodsId, used: e.companyUsed } ) }>{ e.companyUsed ? '不选用' : '选用' }</Button>
           </>
         )
       }
@@ -108,6 +117,38 @@ function PrizeDetail () {
 
     load()
   }, [flag])
+
+  const selectGoods = ({ used, companyGoodsId }) => {
+
+    Modal.confirm({
+      title: '提示',
+      icon: <ExclamationCircleOutlined />,
+      centered: true,
+      content: `确定${ used ? '不选用' : '选用' }商品吗？`,
+      okText: '确定',
+      cancelText: '取消',
+      onCancel: () => {},
+      onOk: async () => {
+
+        try {
+
+          const { state } = await company.selectGoods({
+            used: !used,
+            companyGoodsId,
+          })
+
+          if (!state) return
+
+          setFlag(!flag)
+
+          message.success('操作成功')
+        } catch (error) {
+
+          console.error('~~error~~', error)
+        }
+      }
+    })
+  }
 
   const getGoodsDetail = e => {
 
